21020eba85e0ece5bf32f86bcc31921fb9fdb42e
antic9 <39718108+antic9@users.noreply.github.com>
2022-02-26 14:11:01 +0900

    cleanup

--- init.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker-compose exec database bash -c "chmod 0775 docker-entrypoint-initdb.d/init-database.sh"
-docker-compose exec database bash -c "./docker-entrypoint-initdb.d/init-database.sh"
